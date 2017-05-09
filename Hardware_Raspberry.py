1aeb0661d40cf84ee2756127b2759dd7cad48b3c
mariepius <longliveadw@gmail.com>
2017-05-09 15:00:36 +0800

    Update Hardware_Raspberry.py

--- Hardware_Raspberry.py
+++ Hardware_Raspberry.py
@@ -12,9 +12,10 @@ GPIO.setup(13,GPIO.OUT)
 GPIO.setup(06,GPIO.OUT)
 
 #close all led 
-GPIO.output(26,True)
+GPIO.output(26,False)
 GPIO.output(19,False)
 GPIO.output(13,False)
+GPIO.output(06,False)
 parking_A = False
 parking_B = False
 
@@ -39,7 +40,6 @@ def passes1(my_output):
 	print ('Button1 Pressed')
 	GPIO.output(13,True)
 	
-	# user input (PASS CODE Parking A)
 	parking_slot = raw_input('Key in the parking_slot code number')
 	access_code(parking_slot)
 	GPIO.output(13,False)
@@ -53,15 +53,13 @@ def passes2(my_output):
 	# key in the code 
 	GPIO.output(06,True)
 	
-	#user B input (PASS CODE Parking B)
 	parking_slot = raw_input('Key in the parking_slot code number')
 	access_code(parking_slot)
 	GPIO.output(06,False)
 
 #============================================
-#always loop (main loop)
+
 while True:
-	#initialization 
 	input_state1 = GPIO.input(2)
 	input_state2 = GPIO.input(3)
 	input_state3 = GPIO.input(4)
@@ -69,12 +67,10 @@ while True:
 	my_output2 = False;
 	my_output3 = False;
 
-	# Parking A (check whether have car or not)
-	# my_output1 => indicator => variable pass to Yi Wei
-	# go to passess1 function
+	#parking A
 
 	if input_state1 == False:
-		
+		time.sleep(0.5)
 		if parking_A == False:
 			parking_A = True
 			print ('New Car A Come In')
@@ -86,7 +82,7 @@ while True:
 				
 		
 	else : 
-		
+		time.sleep(0.5)
 		if parking_A == True:
 			parking_A = False
 			GPIO.output(26,False)
@@ -96,18 +92,19 @@ while True:
 		elif parking_A == False:
 			print ('Parking A Available')
 			GPIO.output(26,False)
-	# Parking B
-	#my_output2 => indicator => variable pass to Yi Wei
-	# go to passess2 funciton
 
+
+
+
+	# Parking B 
 	if input_state2 == False:
-		
+		time.sleep(0.5)
 		if parking_B == False:
 			parking_B = True
 			print ('New Car B Come In')
 			my_output2 = 'True' 
 			GPIO.output(19,True)
-			passes1(my_output2)
+			passes2(my_output2)
 		elif parking_B == True:
 			print ('Car B is still parking')
 
@@ -115,14 +112,14 @@ while True:
 
 	else : 
 		
-
+		time.sleep(0.5)
 		if parking_B == True:
 			parking_B = False
 			GPIO.output(19,False)
 			my_output1 = 'False' 
 			print ('Car B is moving out')
 			GPIO.output(19,False)
-		elif parking_A == False:
+		elif parking_B == False:
 			print ('Parking B Available')
 			GPIO.output(19,False)
 
